c346edb6ea9480a3d367f1fa818c9cc15cd3dfa1
SaskatchewanPirate <90709819+SaskatchewanPirate@users.noreply.github.com>
2023-05-14 19:55:51 -0400

    Update debouncer.vhd

--- debouncer.vhd
+++ debouncer.vhd
@@ -1,58 +1,48 @@
--- adapted from Verilog file
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-
+use ieee.std_logic_unsigned.all;
 entity debouncer is
-    PORT (
-        clk : IN STD_LOGIC; -- input clk,
-        I0 : IN STD_LOGIC; -- input I0,
-        I1 : IN STD_LOGIC; -- input I1,
-        O0 : OUT STD_LOGIC REGISTER; -- output reg O0,
-        O1 : OUT STD_LOGIC REGISTER -- output reg O1
+    Port (
+        clk  : in  std_logic;
+        I0   : in  std_logic;
+        I1   : in  std_logic;
+        O0   : out std_logic;
+        O1   : out std_logic
     );
 end debouncer;
 
 architecture Behavioral of debouncer is
-    SIGNAL cnt0 : STD_LOGIC_VECTOR (4 DOWNTO 0) REGISTER; -- reg [4:0]cnt0, cnt1;
-    SIGNAL cnt1 : STD_LOGIC_VECTOR (4 DOWNTO 0) REGISTER;
-    SIGNAL Iv0 : STD_LOGIC REGISTER;
-    SIGNAL Iv1 : STD_LOGIC REGISTER;
-    SIGNAL out0 : STD_LOGIC REGISTER; -- reg out0, out1;
-    SIGNAL out1 : STD_LOGIC REGISTER;
-begin
-    -- reg Iv0=0,Iv1=0;
-    initial : PROCESS
-    BEGIN
-        Iv0 <= '0';
-        Iv1 <= '1';
-        WAIT;
-    END PROCESS;
-    
-    proc : PROCESS -- always@(posedge(clk))begin
-    BEGIN
-        WAIT UNTIL rising_edge(clk);
-        IF (I0 = Iv0) THEN -- if (I0==Iv0)begin
-            IF (TO_INTEGER(UNSIGNED(cnt0)) = 19) THEN -- if (cnt0==19)O0<=I0;
-                O0 <= I0;
-            ELSE -- else cnt0<=cnt0+1;
-                cnt0 <= STD_LOGIC_VECTOR(TO_UNSIGNED(TO_INTEGER(UNSIGNED(cnt0))+1, cnt0'length));
-            END IF; -- end
-        ELSE -- else begin
-            cnt0 <= "00000"; -- cnt0<="00000";
-            Iv0 <= I0; -- Iv0<=I0;
-        END IF;-- end
-        IF (I1 = Iv1) THEN -- if (I1==Iv1)begin
-                IF (TO_INTEGER(UNSIGNED(cnt1)) = 19) THEN -- if (cnt1==19)O1<=I1;
-                    O1 <= I1;
-                ELSE -- else cnt1<=cnt1+1;
-                    cnt1 <= STD_LOGIC_VECTOR(TO_UNSIGNED(TO_INTEGER(UNSIGNED(cnt1))+1, cnt1'length));
-            END IF;-- end
-        ELSE -- else begin
-            cnt1 <= "00000"; -- cnt1<="00000";
-            Iv1 <= I1; -- Iv1<=I1;
-        END IF; -- end
-    END PROCESS; -- end
 
+    signal cnt0, cnt1 : std_logic_vector (4 downto 0);
+    signal Iv0, Iv1 : std_logic := '0';
+    signal out0, out1 : std_logic;
+
+begin
+    process(clk) 
+        begin
+        if rising_edge(clk) then
+            if (I0=Iv0) then
+                if cnt0="10011" then
+                    O0 <= I0;
+                else
+                    cnt0 <= std_logic_vector(unsigned(cnt0)+1);
+                end if;
+            else
+                cnt0 <= "00000";
+                Iv0 <= I0;
+            end if;
+            
+            if (I1=Iv1) then
+                if (cnt1="10011") then
+                    O1 <= I1;
+                else
+                    cnt1 <= std_logic_vector(unsigned(cnt1)+1);
+                end if;
+            else
+                cnt1 <= "00000";
+                Iv1 <= I1;
+            end if;
+        end if;
+    end process;
 end Behavioral;
